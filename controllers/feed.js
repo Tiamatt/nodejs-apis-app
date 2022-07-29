e5125bddce971abe5fd70ee51bf6b0af32f75629
tiamatt <samira.yusifova@gmail.com>
2022-07-28 20:11:07 -0400

    Added request validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator'); // add for validation
+
 // implementation of GET /feed/posts-kali
 exports.getPostsKali = (req, res, next) => {
   const response = {
@@ -8,22 +10,33 @@ exports.getPostsKali = (req, res, next) => {
       }
     ]
   };
-  // HTTP 200 OK success status response code indicates that the request has succeeded
   res.status(200).json(response);
 };
 
 // implementation of POST /feed/post-kali
 exports.createPostKali = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
+  // return error status if request validation is failed
+  // see implementation of validation in routes\feed.js
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors
+      });
+    return;  
+  }
+
+  // return success status otherwise
   const response = {
     message: 'Your post has been created successfully!',
-    post: { 
+    post: {
       id: new Date().toISOString(),
-      title: title, 
-      content: content 
+      title: req.body.title, 
+      content: req.body.content,
+      author: req.body.author
     }
   };
-  // HTTP 201 CREATED success status response code indicates that the request has succeeded and has led to the creation of a resource (e..g in db)
   res.status(201).json(response);
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,24 @@
 const express = require('express');
-
+const { body } = require('express-validator'); // add for validation
 const feedController = require('../controllers/feed');
-
 const router = express.Router();
 
 // GET /feed/posts-kali
 router.get('/posts-kali', feedController.getPostsKali);
 
 // POST /feed/post-kali
-router.post('/post-kali', feedController.createPostKali);
+router.post('/post-kali', [
+    // sanitize request body
+    // express-validator doc:  https://express-validator.github.io/docs/index.html 
+    body('title')
+        .trim()
+        .isLength({ min: 5, max: 50 }),
+    body('content')
+        .trim()
+        .isLength({ min: 20, max: 10000 }),
+    body('author.name')
+        .trim()
+        .isLength({ min: 1, max: 100 }),
+], feedController.createPostKali);
 
 module.exports = router;
